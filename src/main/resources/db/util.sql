ebcf9fd39fb362647fea996fedfbf450ed027128
Abel-Landeo <abel.landeo@gmail.com>
2018-04-23 01:24:54 -0500

    improving Test Plan of JMeter

--- src/main/resources/db/util.sql
+++ src/main/resources/db/util.sql
@@ -1,10 +1,11 @@
 select * from mtc_ticket_number order by id_ticket_number;
 select * from mtc_estado_reclamo;
-select * from mtc_reclamo;
+select * from mtc_reclamo order by id_reclamo;
 
 truncate table mtc_reclamo;
 select nextval('mtc_reclamo_id_reclamo_seq');
 select setval('mtc_reclamo_id_reclamo_seq',1, false);
+update mtc_ticket_number set "number" = 0 where id_ticket_number in (1,2,3);
 
 select public.generateticket();
 
